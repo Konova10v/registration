944af748b749da8cf4eb236877d7fa6be445f9ef
konovalov_k <konova10v@yandex.ru>
2022-12-29 14:54:04 +0500

    konova10v
    create project

---
+++ RegistrationApp/RegistrationApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RegistrationApp
+//
+//  Created by Кирилл Коновалов on 29.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegistrationApp/RegistrationApp/RegistrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationAppApp.swift
+//  RegistrationApp
+//
+//  Created by Кирилл Коновалов on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
